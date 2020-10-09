5a1f603891dbbbccf96cd8aff9a064f34f0a7716
Naokazu Terada <naokazu.terada@gmail.com>
2020-10-10 00:39:11 +0900

    Fix eslint

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const hexToRgb = (hex) => {
   const res = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-  return res ? `rgb(${parseInt(res[1], 16)},${parseInt(res[2], 16)},${parseInt(res[3], 16)})` : null
+  const r = parseInt(res[1], 16)
+  const g = parseInt(res[2], 16)
+  const b = parseInt(res[3], 16)
+  return res ? `rgb(${r},${g},${b})` : null
 }
 module.exports.hexToRgb = hexToRgb
 
 const isValidColor = (color) => {
-  if (typeof color !== 'string') throw new Error(`Wrong parameter type: String / typeof color = ${typeof color}`)
+  if (typeof color !== 'string') {
+    throw new TypeError(
+      `Wrong parameter type: String / typeof color = ${typeof color}`
+    )
+  }
   color = color.toLowerCase().replace(/\s/g, '') // normalize
   // Create test elements and assign styles
   const s = document.createElement('div').style
   s.color = color
   const computed = s.color.replace(/\s/g, '') // normalize
   // First compare with "rgba()". If color is Hex, change to rgb and compare
-  return  computed == color || computed == hexToRgb(color)
+  return computed === color || computed === hexToRgb(color)
 }
 module.exports.isValidColor = isValidColor
